6ed5c0cc46575f2df0daf1cd853cadcdb239af31
jlinke <jan-h.linke@gmx.de>
2024-09-27 18:40:19 +0200

    - initial commit

---
+++ src/main/java/com/wohlgemuth/animepictureapp/AnimePictureAppApplication.java
@@ -0,0 +1,13 @@
+package com.wohlgemuth.animepictureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimePictureAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnimePictureAppApplication.class, args);
+  }
+
+}
